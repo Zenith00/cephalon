3855f428b74fa45bc109569168b13bd05f556c12
Zenith <z@zenith.dev>
2022-04-26 17:58:59 -0700

    fix maths

--- utils/math.ts
+++ utils/math.ts
@@ -1,21 +1,38 @@
-export const make_pmf = (diceFace: number) =>
-  [...Array(diceFace).keys()].map((_) => 1 / diceFace);
+export type PMF = Map<number, number>;
 
-export const convolve_pmfs = (pmfX: number[], pmfY: number[]) => {
-  const mtx = [...Array(pmfX.length).keys()].map((x) =>
-    [...Array(pmfY.length).keys()].map((y) => pmfX[x] * pmfY[y])
+export const boundProb = (x: number) => {
+  return Math.max(0.05, Math.min(0.95, x));
+};
+
+export const make_pmf = (diceFace: number) =>
+  new Map([...Array(diceFace).keys()].map((i) => [i + 1, 1 / diceFace]));
+
+export const convolve_pmfs = (pmfX: PMF, pmfY: PMF, add: boolean) => {
+  const jointProb = [...pmfX.entries()].map(([k, x]) =>
+    [...pmfY.entries()].map(([k, y]) => x.valueOf() * y.valueOf())
   );
-  return [...Array(mtx.length + mtx[0].length).keys()].map((diagDex) =>
-    [...Array(mtx.length + mtx[0].length).keys()]
-      .map((i) => (mtx[diagDex - i] ?? [])[i] ?? 0)
-      .reduce((l, r) => l + r)
+
+  const diagSign = add ? 1 : -1;
+
+  const xLowerBound = Math.min(...pmfX.keys());
+  const yLowerBound = Math.min(...pmfY.keys());
+
+  const diagExtra = add ? 0 : pmfY.size - 1;
+
+  const bound = (x: number) =>
+    x + (add ? xLowerBound + yLowerBound : xLowerBound - pmfY.size);
+
+  return [...Array(pmfX.size + pmfY.size - 1).keys()].reduce(
+    (acc, diagDex) =>
+      acc.set(
+        bound(diagDex),
+        [...Array(pmfX.size + pmfY.size).keys()]
+          .map(
+            (i) => (jointProb[diagDex - diagExtra - diagSign * i] ?? [])[i] ?? 0
+          )
+          .reduce((l, r) => l + r)
+      ),
+    new Map()
   );
   // return diag_sum(matrix);
 };
-
-// export const diag_sum = (mtx: number[][]) =>
-//   [...Array(mtx.length + mtx[0].length).keys()].map((diagDex) =>
-//     [...Array(mtx.length + mtx[0].length).keys()]
-//       .map((i) => (mtx[diagDex - i] ?? [])[i] ?? 0)
-//       .reduce((l, r) => l + r)
-//   );
