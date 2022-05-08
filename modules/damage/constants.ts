e1353957416e7598fd8d8ccc4aa08040fd3210d0
Zenith <z@zenith.dev>
2022-05-07 17:16:45 -0700

    Expand default modifier options

--- modules/damage/constants.ts
+++ modules/damage/constants.ts
@@ -5,3 +5,21 @@ export const ADVANTAGE_TO_DICE = {
   disadvantage: "2d20kl",
   superdisadvantage: "3d20kl",
 };
+let modifierIndex = 0;
+
+export const defaultModifierOptions = [
+  { label: "Bless [+1d4]", value: (modifierIndex++).toString() },
+  { label: "Bane [-1d4]", value: (modifierIndex++).toString() },
+  {
+    label: "S:DS Favored by the Gods [+2d4]",
+    value: (modifierIndex++).toString(),
+  },
+  {
+    label: "C:PC Emboldening Bond [+1d4]",
+    value: (modifierIndex++).toString(),
+  },
+  {
+    label: "D:S Cosmic Omen (Weal) [+1d4]",
+    value: (modifierIndex++).toString(),
+  },
+];
