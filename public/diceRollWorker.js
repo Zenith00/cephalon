ef12b7a71186f65d66ffd59432a0ecd29a6a261c
Zenith <z@zenith.dev>
2022-05-09 08:37:52 -0700

    Prep for complex dice

--- public/diceRollWorker.js
+++ public/diceRollWorker.js
@@ -5,25 +5,15 @@ const SIMS = 1000;
 
 let mean = (arr) => arr.reduce((a, b) => a + b) / arr.length;
 
-let variance = (array, m) => {
-  // let m = mean(array);
-  return mean(arr.map((num) => Math.pow(num - m, 2)));
-};
-
 addEventListener("message", (event) => {
-  let { damage } = event.data;
+  let { damage: dice } = event.data;
 
   const rolls = [...Array(SIMS).keys()]
-    .map((x) => roller.rollValue(damage))
+    .map((x) => roller.rollValue(dice))
     .reduce((acc, curr) => acc.set(curr, (acc.get(curr) || 0) + 1), new Map());
 
-  // const rolls = Array.from({ length: SIMS }, (x, i) => i + 1).map(() =>
-  //   roller.rollValue(event.data.damage)
-  // );
-
-  // const m = mean(rolls);
   postMessage([
     event.data.index,
-    new Map([...rolls.entries()].map((k, v) => [k, v / SIMS])),
+    new Map([...rolls.entries()].map(([k, v]) => [k, v / SIMS])),
   ]);
 });
