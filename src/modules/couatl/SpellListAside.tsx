0308c2c8d164f4c0e2ff002386d3a8d29e1c7df5
Zenith <z@zenith.dev>
2022-06-10 16:50:20 -0700

    Initial couatl updates

---
+++ src/modules/couatl/SpellListAside.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Accordion, Aside, ScrollArea, Text,
+} from '@mantine/core';
+import type { CreatureName, SpellName } from '@couatl/types';
+import CreatureList from '@condition/CreatureList';
+
+const SpellListAside = ({
+  spells,
+  hidden,
+  jumpTo,
+}: {
+  spells: Record<SpellName, CreatureName[]>;
+  hidden: boolean;
+  jumpTo: ({ creature, spell }: {creature: CreatureName, spell: SpellName}) => void
+}) => (
+  <Aside
+    p="md"
+    width={{
+      sm: 250, md: 250, lg: 275, xl: 425,
+    }}
+    hiddenBreakpoint="sm"
+    hidden={hidden}
+  >
+
+    <Aside.Section component={ScrollArea} grow>
+      {spells && Object.entries(spells).map(([name, source]) => (
+        <Accordion>
+          <Accordion.Item label={name}>
+            {source.map((sourceCreature) => <Text onClick={() => jumpTo({ creature: sourceCreature, spell: name })}>{sourceCreature}</Text>)}
+          </Accordion.Item>
+        </Accordion>
+      ))}
+    </Aside.Section>
+  </Aside>
+);
+
+export default SpellListAside;
