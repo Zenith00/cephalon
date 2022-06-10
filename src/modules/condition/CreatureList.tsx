a030eb7b69a7dc0e021dd9c81cc15b1ce24bdb8c
Zenith <z@zenith.dev>
2022-06-10 16:50:42 -0700

    Adjust creature list sizing

--- src/modules/condition/CreatureList.tsx
+++ src/modules/condition/CreatureList.tsx
@@ -3,7 +3,7 @@ import {
   Aside, ScrollArea, Text, Title,
 } from '@mantine/core';
 
-function CreatureList({
+const CreatureList = ({
   creatures,
   hidden,
   selected,
@@ -11,38 +11,35 @@ function CreatureList({
   creatures: string[];
   hidden: boolean;
   selected?: { condition: string; creatureType: string };
-}) {
-  return (
-    <Aside
-      p="md"
-      width={{
-        sm: 250, md: 250, lg: 300, xl: 450,
-      }}
-      hiddenBreakpoint="sm"
-      hidden={hidden}
-      // style={{width: "20%"}}
+}) => (
+  <Aside
+    p="md"
+    width={{
+      sm: 250, md: 250, lg: 275, xl: 425,
+    }}
+    hiddenBreakpoint="sm"
+    hidden={hidden}
+  >
+    {selected && (
+    <Title
+      order={3}
     >
-      {selected && (
-        <Title
-          order={3}
+      {`${selected.creatureType[0].toLocaleUpperCase()}${selected.creatureType.substring(
+        1,
+      )}s immune to ${selected.condition.toLocaleLowerCase()}:`}
+    </Title>
+    )}
+    <Aside.Section component={ScrollArea} grow>
+      {creatures.map((c) => (
+        <Text
+          key={c}
+          style={{ fontSize: hidden ? '1vw' : '20px', whiteSpace: 'nowrap' }}
         >
-          {`${selected.creatureType[0].toLocaleUpperCase()}${selected.creatureType.substring(
-            1,
-          )}s immune to ${selected.condition.toLocaleLowerCase()}:`}
-        </Title>
-      )}
-      <Aside.Section component={ScrollArea} grow>
-        {creatures.map((c) => (
-          <Text
-            key={c}
-            style={{ fontSize: hidden ? '1vw' : '20px', whiteSpace: 'nowrap' }}
-          >
-            {c}
-          </Text>
-        ))}
-      </Aside.Section>
-    </Aside>
-  );
-}
+          {c}
+        </Text>
+      ))}
+    </Aside.Section>
+  </Aside>
+);
 
 export default CreatureList;
