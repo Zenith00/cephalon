d84d8b90829d638c4bfff7bdf3eb50d6d658c0bc
Zenith <z@zenith.dev>
2022-06-10 16:51:52 -0700

    Add advanced mode toggle

--- src/modules/damage/TargetNavbar.tsx
+++ src/modules/damage/TargetNavbar.tsx
@@ -5,40 +5,62 @@ import {
   Navbar,
   NumberInput,
   Space,
-  Title,
+  Switch,
+  Title, Tooltip,
 } from '@mantine/core';
+import type { SetState } from '@common';
 
 import type { Target } from '@pages/Damage';
+import { InfoCircle } from 'tabler-icons-react';
 
-function TargetNavbar({
+const TargetNavbar = ({
   target,
   setTarget,
+  advancedMode,
+  setAdvancedMode,
 }: {
   target: Target;
-  setTarget: React.Dispatch<React.SetStateAction<Target>>;
-}) {
-  return (
-    <Navbar width={{ base: 200 }} py="xs" p="xs">
-      <Box px="2px">
-        <Navbar.Section>
-          <Title order={4}>Target Info</Title>
-          <NumberInput
-            label="AC"
-            step={1}
-            value={target.ac}
-            onChange={(val) => setTarget({ ...target, ac: val || 0 })}
-          />
-        </Navbar.Section>
-        <Space h="md" />
-        {/* <Divider pt={"3px"} size={"sm"}/> */}
-        {/* <Navbar.Section> */}
-        {/*  <Title order={4}>Special</Title> */}
-        {/*  <Checkbox label={"Elven Accuracy"} /> */}
-        {/*  <Checkbox label={"Lucky"} /> */}
-        {/* </Navbar.Section> */}
-      </Box>
-    </Navbar>
-  );
-}
+  setTarget: SetState<Target>;
+  advancedMode: boolean,
+  setAdvancedMode: SetState<boolean>
+}) => (
+  <Navbar width={{ base: 200 }} py="xs" p="xs">
+    <Box px="2px">
+      <Navbar.Section>
+        <Title order={4}>Target Info</Title>
+        <NumberInput
+          label="AC"
+          step={1}
+          value={target.ac}
+          onChange={(val) => setTarget({ ...target, ac: val || 0 })}
+        />
+      </Navbar.Section>
+      <Space h="md" />
+
+      <Navbar.Section>
+        <Title order={4}>Config</Title>
+        <div
+          style={{ display: 'flex', alignItems: 'center', height: '100%' }}
+        >
+          <Switch label="Advanced Mode" checked={advancedMode} onChange={(ev) => setAdvancedMode(ev.currentTarget.checked)} />
+          <Tooltip
+            label="Provides a complex damage output"
+            pl={4}
+            mt={4}
+          >
+            <InfoCircle size={16} />
+          </Tooltip>
+        </div>
+      </Navbar.Section>
+
+      {/* <Divider pt={"3px"} size={"sm"}/> */}
+      {/* <Navbar.Section> */}
+      {/*  <Title order={4}>Special</Title> */}
+      {/*  <Checkbox label={"Elven Accuracy"} /> */}
+      {/*  <Checkbox label={"Lucky"} /> */}
+      {/* </Navbar.Section> */}
+    </Box>
+  </Navbar>
+);
 
 export default TargetNavbar;
