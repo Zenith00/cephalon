9ca6c09cf656f73b516e378977c4b992be468e03
Zenith <z@zenith.dev>
2022-07-02 12:17:53 -0700

    Add CSP headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
+const ContentSecurityPolicy = `
+  worker-src 'self';
+`
+const securityHeaders = [{
+  key: 'Content-Security-Policy',
+  value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+}];
+
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 };
